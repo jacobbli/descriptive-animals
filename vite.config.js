c807476c0cd4ccf296c20cb92cf5cfca914b431d
jacobli <hello@jacobli.ca>
2025-03-16 16:03:36 +0800

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: 'https://jacobbli.github.io/descriptive-animals/',
+  base: '/descriptive-animals/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
